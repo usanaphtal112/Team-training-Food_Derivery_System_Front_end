5ea3568473aad8b85cfef01a37ff4b34d4525884
usanaphtal112 <usanaphtal112@gmail.com>
2023-05-11 20:23:43 +0300

    Sliding picture added in  homepage

---
+++ homepage.js
@@ -0,0 +1,14 @@
+const slidesContainer = document.querySelector('.slides');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+function slidePrev() {
+  slidesContainer.appendChild(slidesContainer.firstElementChild);
+}
+
+function slideNext() {
+  slidesContainer.prepend(slidesContainer.lastElementChild);
+}
+
+prevButton.addEventListener('click', slidePrev);
+nextButton.addEventListener('click', slideNext);
